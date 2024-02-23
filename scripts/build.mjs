fb08523a27d5496858c7ca82cde02ab3a2a6f8f2
wolfy1339 <webmaster@wolfy1339.com>
2024-02-23 09:40:42 -0500

    fix(build): remove NodeJS/Browser specific builds
    The 2 builds are the same in this case. It fixes usage with the `moduleResolution` option set to `bundler`

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -33,30 +33,6 @@ async function main() {
     await rm(typeFile);
   }
 
-  const entryPoints = ["./pkg/dist-src/index.js"];
-
-  await Promise.all([
-    // Build the a CJS Node.js bundle
-    esbuild.build({
-      entryPoints,
-      outdir: "pkg/dist-node",
-      bundle: true,
-      platform: "node",
-      target: "node18",
-      format: "esm",
-      ...sharedOptions,
-    }),
-    // Build an ESM browser bundle
-    esbuild.build({
-      entryPoints,
-      outdir: "pkg/dist-web",
-      bundle: true,
-      platform: "browser",
-      format: "esm",
-      ...sharedOptions,
-    }),
-  ]);
-
   // Copy the README, LICENSE to the pkg folder
   await copyFile("LICENSE", "pkg/LICENSE");
   await copyFile("README.md", "pkg/README.md");
@@ -76,18 +52,12 @@ async function main() {
         files: ["dist-*/**", "bin/**"],
         exports: {
           ".": {
-            node: {
-              types: "./dist-types/index.d.ts",
-              import: "./dist-node/index.js",
-            },
-            browser: {
-              types: "./dist-types/index.d.ts",
-              import: "./dist-web/index.js",
-            }
+            types: "./dist-types/index.d.ts",
+            import: "./dist-src/index.js",
           }
         },
         sideEffects: false,
-        unpkg: "dist-web/index.js",
+        unpkg: "dist-src/index.js",
       },
       null,
       2
