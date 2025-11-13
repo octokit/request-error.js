327afe43e3f78665a22ee933237bf8902a10666c
wolfy1339 <webmaster@wolfy1339.com>
2025-11-13 12:05:20 -0500

    style: prettier

--- src/types.ts
+++ src/types.ts
@@ -3,4 +3,4 @@ import type { RequestOptions, OctokitResponse } from "@octokit/types";
 export interface RequestErrorOptions extends ErrorOptions {
   response?: OctokitResponse<unknown> | undefined;
   request: RequestOptions;
-};
+}
